c4be9ca713735df58f1fa9860368e1f00b90faf4
pnaessen <pnaessen@student.42lyon.fr>
2026-02-02 20:07:35 +0100

    refactor: improve error handling and initialize ping packet in main function

--- src/main.c
+++ src/main.c
@@ -8,27 +8,34 @@ int main(int argc, char const **argv)
 	return 1;
     }
 
+    t_ping_packet pkt;
     struct sockaddr_in sockaddr;
+    uint16_t  seq = 0;
 
     if (resolve_dns(argv[1], &sockaddr) != 0) {
 	dprintf(2, "Error in dns resolution\n");
-	return 1;
+	return ERR_DNS;
     }
 
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sockfd < 0) {
 	dprintf(2, "Error creat socket\n");
-	return 1;
+	return ERR_SOCKET;
     }
-    struct icmphdr icmp_req;
-    memset(&icmp_req, 0, sizeof(icmp_req));
-    setIcmpHdr(&icmp_req);
 
-    printf("checksum %d\n", icmp_req.checksum);
-    printf("type %d\n", icmp_req.type);
-    printf("code %d\n", icmp_req.code);
-    printf("id %d\n", icmp_req.un.echo.id);
+    while (/*signal up*/ true) {
+	init_ping_packet(&pkt, seq);
+	// sendto()
+	// recvfrom()
+	seq++;
+    usleep(PING_INTERVAL);
+    }
 
-	printf("Pas de segfault\n");
-    return 0;
+    // printf("checksum %d\n", icmp_req.checksum);
+    // printf("type %d\n", icmp_req.type);
+    // printf("code %d\n", icmp_req.code);
+    // printf("id %d\n", icmp_req.un.echo.id);
+
+    printf("Pas de segfault\n");
+    return SUCCESS;
 }
